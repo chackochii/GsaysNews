4354e55e0073de9843ee66d30c666cfa657b8a32
EDWIN CHACKO <163020056+chackochii@users.noreply.github.com>
2025-12-31 00:17:12 +0530

    Refactor news routes to simplify image upload handling

--- backend3/src/modules/news/news.routes.js
+++ backend3/src/modules/news/news.routes.js
@@ -1,17 +1,13 @@
 import express from 'express';
-import { createNews, listNewsByCategory, editNews, listNews ,getCategories, addCategory, deleteNews, listOneNews } from '../news/news.contoller.js';
-import { multerUpload, uploadToGCS } from "../../config/s3.config.js";
+import { createNews, listNewsByCategory, editNews, listNews,addCategory,getCategories , deleteNews } from '../news/news.contoller.js';
+import { upload } from '../../config/s3.config.js';
 
 const router = express.Router();
+
 router.get('/all', listNews);
-router.post('/create',multerUpload.single("image"), uploadToGCS, createNews);
+router.post('/create', upload.single('image'), createNews);
 router.get('/category/:category', listNewsByCategory);
-router.put('/edit/:id',multerUpload.single("image"), uploadToGCS, editNews);
+router.put('/edit/:id', upload.single('image'), editNews);
 router.post('/addCategory', addCategory);
 router.get('/categories', getCategories);
 router.delete('/delete/:id', deleteNews);
-
-router.get('/:id', listOneNews);
-
-
-export default router;
